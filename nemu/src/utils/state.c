8a27c00d8cc01f3f860189965a961242e3a23f9e
tracer-ics2022 <tracer@njuics.org>
2022-09-18 20:21:00 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:21:00 up 59 min,  1 user,  load average: 0.32, 0.17, 0.12

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -20,7 +20,7 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
-		printf("nemu_state.state == NEMU_QUIT\n");
+		printf("%d\n",nemu_state.state == NEMU_QUIT);
 //		printf("!good = %d, good = %d\n", !good, good);
   return !good;
 }
