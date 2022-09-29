009243121b9f43f14dd6e646ba752970b212e365
tracer-ics2022 <tracer@njuics.org>
2022-09-29 16:33:29 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:33:29 up  2:19,  3 users,  load average: 0.30, 0.25, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -364,15 +364,15 @@ word_t eval(int sta, int end){
 		if(op == -1)
 	  {
 			valid_expr = false;
-	  	//printf("HERE A\n");
+	  	printf("HERE A\n");
 			return 0;
 		}
 		int op_type = tokens[op].type;
-		//printf("area1: %d, %d\narea2: %d, %d\n", sta, op-1,op+1,end);
+		printf("area1: %d, %d\narea2: %d, %d\n", sta, op-1,op+1,end);
 		word_t val1 = eval(sta, op - 1);
 		word_t val2 = eval(op + 1, end);
-		//printf("val1 = %u, val2 = %u\n", val1, val2);
-		//printf("type now = %d\n", op_type);
+		printf("val1 = %u, val2 = %u\n", val1, val2);
+		printf("type now = %d\n", op_type);
 		switch(op_type){
 			case '+':	return val1 + val2;
 			case '-': return val1 - val2;
