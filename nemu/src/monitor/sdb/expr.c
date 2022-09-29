0e2d38c20489aa611f8c83d156377fadfba05c7e
tracer-ics2022 <tracer@njuics.org>
2022-09-29 15:27:57 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:27:57 up  1:14,  3 users,  load average: 0.00, 0.07, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -373,8 +373,8 @@ word_t eval(int sta, int end){
 	//	printf("area1: %d, %d\narea2: %d, %d\n", sta, op-1,op+1,end);
 		word_t val1 = eval(sta, op - 1);
 		word_t val2 = eval(op + 1, end);
-	//	printf("val1 = %u, val2 = %u\n", val1, val2);
-	//	printf("type now = %d\n", op_type);
+		printf("val1 = %u, val2 = %u\n", val1, val2);
+		printf("type now = %d\n", op_type);
 		switch(op_type){
 			case '+':	return val1 + val2;
 			case '-': return val1 - val2;
