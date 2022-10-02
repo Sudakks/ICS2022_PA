46fc2f9201261cb8dfc5abda78885bfb5ab57657
tracer-ics2022 <tracer@njuics.org>
2022-10-02 11:29:35 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:29:35 up  1:37,  3 users,  load average: 0.23, 0.14, 0.15

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -58,12 +58,12 @@ WP* new_wp(char* e)
 		WP* fr = free_;
 		//fr->expression = e;
 		//赋值直接！
-		int idx = 0;
-		while(e[idx] != '\0')
+		//int idx = 0;
+		/*while(e[idx] != '\0')
 		{
 			*(fr->expression + idx) = e[idx];
 			idx++;
-		}
+		}*/
 		fr->suc = malloc(sizeof(bool));
 		*(fr->suc) = true;
 		fr->before = expr(fr->expression, fr->suc);
