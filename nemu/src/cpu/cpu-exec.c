98d1ec39185ef34286b37da9a316e91bd49072a8
tracer-ics2022 <tracer@njuics.org>
2022-10-16 21:29:16 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:29:16 up  2:18,  3 users,  load average: 0.02, 0.05, 0.06

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -39,9 +39,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }//Here print the information of step into!
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 	//scan all the watchpoints
-#ifdef CONFIG_WATCHPOINT
+//#ifdef CONFIG_WATCHPOINT
 	scan_wps();
-#endif
+//#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
