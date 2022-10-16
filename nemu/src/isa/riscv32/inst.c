8d949b1fabc24fa3bf44838e16e7ad7187f8bf5e
tracer-ics2022 <tracer@njuics.org>
2022-10-16 15:46:35 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:46:35 up  1:14,  3 users,  load average: 0.16, 0.17, 0.11

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -46,7 +46,7 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
-		case TYPE_JAL:								 immJAL(); printf("imm = %p\n", imm);break;
+		case TYPE_JAL:								 immJAL(); printf("imm = %u\n", *imm);break;
   }
 }
 
@@ -82,6 +82,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
 
+printf("pc = %u\n", s->pc);
   R(0) = 0; // reset $zero to 0
 
   return 0;
