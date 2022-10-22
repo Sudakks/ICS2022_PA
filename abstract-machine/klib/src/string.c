86f79a1159025747680002f846987525ae0bd2b9
tracer-ics2022 <tracer@njuics.org>
2022-10-22 11:02:33 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:02:33 up  1:00,  3 users,  load average: 0.16, 0.11, 0.12

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -63,7 +63,7 @@ int strcmp(const char *s1, const char *s2) {
 int strncmp(const char *s1, const char *s2, size_t n) {
 	if(n <= 0)
 		return 0;
-	while(--n && *s1 != '\0' && *s1 == *s2)
+	while(--n && *s1 != '\0' && *(unsigned char*)s1 == *(unsigned char*)s2)
 	{
 		s1++;
 		s2++;
