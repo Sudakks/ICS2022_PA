d9cdf9a27c2034653adbfbd763f2fd650d58ad6a
tracer-ics2022 <tracer@njuics.org>
2022-10-22 10:24:18 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:24:18 up 22 min,  3 users,  load average: 0.16, 0.16, 0.21

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -52,7 +52,7 @@ char *strcat(char *dst, const char *src) {
 
 int strcmp(const char *s1, const char *s2) {
 	assert(s1 != NULL && s2 != NULL);
-	while(*s1 != '\0' && *s1 == *s2)
+	while(*s1 && *(unsigned char*)s1 == *(unsigned char*)s2)
 	{
 		s1++;
 		s2++;
