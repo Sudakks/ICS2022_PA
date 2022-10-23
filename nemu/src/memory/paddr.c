f2b1f0aada5bffcfa409e5596a2ed6726a9da4fc
tracer-ics2022 <tracer@njuics.org>
2022-10-23 11:45:17 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:45:17 up  2:26,  3 users,  load average: 0.38, 0.20, 0.12

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -13,11 +13,11 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#include <cpu/cpu.h>
 #include <memory/host.h>
 #include <memory/paddr.h>
 #include <device/mmio.h>
 #include <isa.h>
+#include <common.h>
 void ringbuff_print();
 #if   defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
