1eb2d2ee2d91403096edc0a451eacb2e1af2a9bc
tracer-ics2022 <tracer@njuics.org>
2022-10-23 15:48:50 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:48:50 up 14 min,  3 users,  load average: 0.34, 0.35, 0.33

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -110,8 +110,8 @@ static void exec_once(Decode *s, vaddr_t pc) {
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
-	iringbuff_add(s);
-	//iringbuff_print();
+	//iringbuff_add(s);
+	iringbuff_print();
 	//add instruction to ringbuff every time it has its logbuf
 #endif
 }
