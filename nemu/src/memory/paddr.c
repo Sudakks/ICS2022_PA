d10dc43d45ab951377b8cec3de0ec6356a6996c4
tracer-ics2022 <tracer@njuics.org>
2022-10-25 09:02:15 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:02:15 up 22 min,  3 users,  load average: 0.15, 0.13, 0.19

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -63,7 +63,7 @@ word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) 
 	{
 		word_t ret = pmem_read(addr, len);
-		printf("addr: %#x, len: %d, read: %u\n", addr, len, ret);
+		printf("--- addr: %#x, len: %d, read: %u ---\n", addr, len, ret);
 		return ret; 
 	}
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -74,10 +74,10 @@ word_t paddr_read(paddr_t addr, int len) {
 void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr)))
 	{ 
-		printf("addr: %#x, len: %d, write: %u", addr, len, data);
+		printf("--- addr: %#x, len: %d, write: %u ---\n", addr, len, data);
 		pmem_write(addr, len, data); 
 		return; 
-	}
+	} 
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
