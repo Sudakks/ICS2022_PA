fccaca8eae2c8b707e4bd615ba832c954ba02c91
tracer-ics2022 <tracer@njuics.org>
2022-10-27 12:26:35 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:26:35 up  2:28,  1 user,  load average: 0.23, 0.38, 0.23

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -20,15 +20,15 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 	//compare reg value and PC
-	if(ref_r -> pc != pc)
-		return false;
+	//if(ref_r -> pc != pc)
+	//	return false;
 //	int len = sizeof(regs) / sizeof(regs[0]);
-	/*int len = 32;
+	int len = 32;
 	for(int i = 0; i < len; i++)
 	{
 		if(cpu.gpr[i] != ref_r->gpr[i])
 			return false;
-	}*/
+	}
 	return true;
 }
 
