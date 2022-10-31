a653cb088ed164441786dbd44c5d85ee1722dc81
tracer-ics2022 <tracer@njuics.org>
2022-10-31 21:33:41 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:33:41 up  2:27,  3 users,  load average: 0.36, 0.81, 0.90

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,13 +4,14 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-	uint32_t a = inl(RTC_ADDR);
-	if(a != AM_KEY_NONE)
+	/*
+	kbd->keycode = inl(KBD_ADDR);
+	if(kbd->keycode != AM_KEY_NONE)
 	{
 		putch('y');
 	}
 //	kbd->keycode = inl(KBD_ADDR);
-/*	if(kbd->keycode == AM_KEY_NONE)
+	if(kbd->keycode == AM_KEY_NONE)
 		kbd->keydown = false;
 	else
 		kbd->keydown = true;*/
