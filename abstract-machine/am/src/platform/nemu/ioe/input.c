e1b02cbf2b4ac6c984f6ad34716eeb09de909d12
tracer-ics2022 <tracer@njuics.org>
2022-10-31 22:16:46 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:16:46 up  3:11,  3 users,  load average: 0.19, 0.14, 0.14

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,15 +4,11 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-	/*
-	kbd->keycode = inl(KBD_ADDR);
-	if(kbd->keycode != AM_KEY_NONE)
-	{
-		putch('y');
-	}
+	
+	kbd->keycode = inb(KBD_ADDR);
 //	kbd->keycode = inl(KBD_ADDR);
 	if(kbd->keycode == AM_KEY_NONE)
 		kbd->keydown = false;
 	else
-		kbd->keydown = true;*/
+		kbd->keydown = true;
 }
--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -73,6 +73,11 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
+/*	if(io_read(AM_GPU_FBDRAW).sync == true)
+	{
+		update_screen();
+    io_write(AM_GPU_FBDRAW, 0, 0, vmem, screen_width(), screen_height(), false);
+	}*/
 }
 
 void init_vga() {
