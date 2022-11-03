12d20d3d87b742a0971816765c51604600504a6c
tracer-ics2022 <tracer@njuics.org>
2022-11-03 21:27:33 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:27:33 up  2:37,  3 users,  load average: 0.76, 0.30, 0.14

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -24,6 +24,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+	printf("sync = %d\n", ctl->sync);
   if (ctl->sync) {
     //outl(SYNC_ADDR, 1);
 		int x = ctl->x;
