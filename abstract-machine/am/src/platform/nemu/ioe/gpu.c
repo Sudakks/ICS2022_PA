775516187d864cb29ad1308f01bc723b85e24ebe
tracer-ics2022 <tracer@njuics.org>
2022-11-04 09:39:42 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:39:42 up 40 min,  3 users,  load average: 0.10, 0.08, 0.09

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,14 +5,14 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-	/*
+	
 	int i;
 	uint32_t info = inl(VGACTL_ADDR);	
   int w = (info >> 16) & 0x0000ffff;  // TODO: get the correct width
   int h = (info & 0x0000ffff);  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);*/
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -44,7 +44,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 			for(int j = 0; j < w; j++)
 			{
 				fb[base + j + i * W] = *p;
-				//p++;?neccessary
+				p++;//neccessary
 			}
 		}
   if (ctl->sync) {
