bfd4e859724757fbc3881eb51ab72e8629e2abf3
tracer-ics2022 <tracer@njuics.org>
2022-12-06 21:53:33 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:53:33 up  2:02,  1 user,  load average: 0.26, 0.21, 0.18

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -62,8 +62,6 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-	if(count == 1)
-		count += 5;
 	return _syscall_(SYS_write, fd, (intptr_t) buf, count);
   //_exit(SYS_write);
   //return 0;
