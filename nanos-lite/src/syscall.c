f56968592373950332b85a27bb0f5711885d300e
tracer-ics2022 <tracer@njuics.org>
2022-12-06 16:17:29 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:17:29 up 13 min,  1 user,  load average: 0.04, 0.29, 0.39

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -15,6 +15,26 @@ void sys_exit(Context *c)
 	halt(0);
 }
 
+void sys_write(Context *c)
+{
+	int fd = c->GPR2;
+	char *buf = (char*)(c->GPR3);
+	size_t count = c->GPR4;
+
+	if(fd == 1 || fd == 2)
+	{
+		//stand for stdout, stderr
+		printf("count = %d\n", count);
+		for(size_t i = 0; i < count ; i++)
+		 {
+			putch(*buf);
+			buf++;
+		}
+		//set return value
+		c->GPRx = count;
+	}
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -23,6 +43,9 @@ void do_syscall(Context *c) {
 		sys_yield(c);
 	case SYS_exit:
 		sys_exit(c);
+		break;
+	case SYS_write:
+		sys_write(c);
 		break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
