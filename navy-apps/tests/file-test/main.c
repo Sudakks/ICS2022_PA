af5f79c5dc53ca67fbc3a99ac6e0f85ee6ffc724
tracer-ics2022 <tracer@njuics.org>
2022-12-09 21:03:48 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:03:48 up  2:26,  2 users,  load average: 0.44, 0.29, 0.19

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -14,7 +14,6 @@ int main() {
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
-    printf("n = %d, i = %d\n", n, i);
     assert(n == i + 1);
   }
 
@@ -25,6 +24,7 @@ int main() {
 
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
+    printf("n = %d, i = %d\n", n, i);
     assert(n == i + 1);
   }
 
