9df3e74a35e76447c2f6f4aa6e1f5eeab5758bb0
tracer-ics2022 <tracer@njuics.org>
2022-12-09 19:23:40 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:23:40 up 46 min,  1 user,  load average: 0.27, 0.23, 0.18

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -2,22 +2,21 @@
 #include <assert.h>
 
 int main() {
-printf("www\n");
+
   FILE *fp = fopen("/share/files/num", "r+");
-  printf("qq\n");
   assert(fp);
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
-printf("hereA\n");
+
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-printf("hereB\n");
+
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
@@ -27,7 +26,7 @@ printf("hereB\n");
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-printf("hereC\n");
+
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
