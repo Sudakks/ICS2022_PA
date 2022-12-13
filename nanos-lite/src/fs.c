8651fc42c6f781454edef109c457a78a305ac0d2
tracer-ics2022 <tracer@njuics.org>
2022-12-13 21:12:00 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:12:00 up  1:06,  1 user,  load average: 0.20, 0.18, 0.14

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -37,6 +37,7 @@ static Finfo file_table[] __attribute__((used)) = {
 		在strcmp时会为NULL
 	*/
 	/*[FD_KBD]*/     {"/dev/events", 0, 0, events_read, invalid_write},
+	{"proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
 	
 #include "files.h"
 };
@@ -53,6 +54,7 @@ void init_fs() {
 
 int fs_open(const char *pathname, int flags, int mode)
 {
+	printf("here\n");
 	int file_table_sz = sizeof(file_table) / sizeof(Finfo);
 	//find the same filename in file_table
 	for(int i = 0; i < file_table_sz; i++)
@@ -61,6 +63,7 @@ int fs_open(const char *pathname, int flags, int mode)
 		if(strcmp(tmp, pathname) == 0)
 		{
 			//equal and find
+			printf("i = %d\n", i);
 			open_offset[i] = 0;
 			return i;
 		}
