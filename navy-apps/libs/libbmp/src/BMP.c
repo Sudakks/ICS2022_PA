fe48f21ca618c782cc75d82c2e86805e7b96826c
tracer-ics2022 <tracer@njuics.org>
2022-12-13 23:50:40 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:50:40 up 18 min,  1 user,  load average: 0.27, 0.26, 0.27

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -37,6 +37,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   int line_off = (w * 3 + 3) & ~0x3;
   for (int i = 0; i < h; i ++) {
     fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
+    printf("C\n");
     int nread = fread(&pixels[w * i], 3, w, fp);
     for (int j = w - 1; j >= 0; j --) {
       uint8_t b = *(((uint8_t*)&pixels[w * i]) + 3 * j);
@@ -45,7 +46,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
     }
   }
-printf("C\n");
+printf("D\n");
   fclose(fp);
   if (width) *width = w;
   if (height) *height = h;
