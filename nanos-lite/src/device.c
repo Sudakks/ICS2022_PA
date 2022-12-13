78fa61f301629586f98615f42fe06f41386a0acb
tracer-ics2022 <tracer@njuics.org>
2022-12-13 22:10:48 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:10:48 up  2:05,  1 user,  load average: 0.40, 0.31, 0.20

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -51,6 +51,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 	int w = io_read(AM_GPU_CONFIG).width;
   int h = io_read(AM_GPU_CONFIG).height;
+	printf("----\n");
 	printf("w = %d, h = %d\n", w, h);
 	char* W = "400";
 	char* H = "300";
