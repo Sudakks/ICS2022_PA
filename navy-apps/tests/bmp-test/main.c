70975a2000054a54b92a74d0919ef5d2ec46c736
tracer-ics2022 <tracer@njuics.org>
2022-12-14 12:12:45 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:12:45 up  2:52,  1 user,  load average: 0.21, 0.22, 0.24

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -7,7 +7,9 @@
 int main() {
   NDL_Init(0);
   int w, h;
+  printf("reach here\n");
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  printf("not here\n");
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
   NDL_DrawRect(bmp, 0, 0, w, h);
