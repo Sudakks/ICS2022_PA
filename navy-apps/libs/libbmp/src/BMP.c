90e24e903f5d24441d716e3026bfbd543214c601
tracer-ics2022 <tracer@njuics.org>
2022-12-14 14:35:45 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:35:45 up 50 min,  1 user,  load average: 0.36, 0.30, 0.28

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -24,9 +24,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   if (!fp) return NULL;
   struct BitmapHeader hdr;
   assert(sizeof(hdr) == 54);
-	printf("fread before\n");
   assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
-	printf("fread after\n");
 
   if (hdr.bitcount != 24) return NULL;
   if (hdr.compression != 0) return NULL;
