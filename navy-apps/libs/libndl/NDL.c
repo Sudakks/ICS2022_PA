0a6c4e77a1aa722b20f6e3ff80ba14c9d525383d
tracer-ics2022 <tracer@njuics.org>
2022-12-14 17:04:09 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:04:09 up  3:18,  1 user,  load average: 0.50, 0.18, 0.05

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -37,6 +37,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 		printf("Set size is bigger than screen size\n");
 	final_w = *w;
 	final_h = *h;
+	printf("huabu = %d %d\n", final_w, final_h);
 	//记录最后的画布大小
 
   if (getenv("NWM_APP")) {
