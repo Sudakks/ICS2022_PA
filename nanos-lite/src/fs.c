fd815ebd005f8bf506001b0f128e36c36bf10d2f
tracer-ics2022 <tracer@njuics.org>
2022-12-15 15:03:21 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:03:21 up 49 min,  1 user,  load average: 0.63, 0.41, 0.33

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -55,6 +55,7 @@ int fs_open(const char *pathname, int flags, int mode)
 {
 	printf("in open %s\n", pathname);
 	int file_table_sz = sizeof(file_table) / sizeof(Finfo);
+	printf("eeee\n");
 	//find the same filename in file_table
 	for(int i = 0; i < file_table_sz; i++)
 	{
