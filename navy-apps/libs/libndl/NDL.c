31a1b07d219c806ab71bc840e84a9464ffb763d0
tracer-ics2022 <tracer@njuics.org>
2022-12-15 20:13:03 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:13:03 up 52 min,  1 user,  load average: 0.36, 0.20, 0.23

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -68,7 +68,8 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
 	for(int i = 0; i < h; i++)
 	{
 		lseek(fd, (y + i + off_h) * screen_w + x + off_w, SEEK_SET);
-		write(fd, pixels + (y + i) * final_w + x, w);
+		write(fd, pixels + w * i, w);
+		//write(fd, pixels + (y + i) * final_w + x, w);
 	}
 	close(fd);
 }
