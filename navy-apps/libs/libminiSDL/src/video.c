596dc84096f8fe18b8aacc32152a6ee10ef00cb8
tracer-ics2022 <tracer@njuics.org>
2022-12-15 19:53:50 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:53:50 up 32 min,  1 user,  load average: 0.38, 0.35, 0.28

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
@@ -24,6 +25,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 	{
 		src_x = 0, src_y = 0;
 		w = src->w, h = src->h;
+		printf("scr NULL\n");
 	}
 	else
 	{
@@ -63,6 +65,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 		w = dstrect->w, h = dstrect->h;
 	}
 	//advance pixels
+	/*
 	for(int i = 0; i < h; i++)
 	{
 		for(int j = 0; j < w; j++)
@@ -70,12 +73,13 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 			dst->pixels[(y + i) * dst->w + x + j] = color;
 		}
 	}
-	/*
+	*/
 	for(int i = 0; i < h; i++)
 	{
 		memset(dst->pixels + (y + i) * dst->w + x, color, w);
 	}
-	*/
+	
+
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
