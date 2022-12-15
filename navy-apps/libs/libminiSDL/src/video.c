cdf55d7183ff651cc41ce7246e73ad18cd9e3769
tracer-ics2022 <tracer@njuics.org>
2022-12-15 21:10:03 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:10:03 up  1:49,  1 user,  load average: 0.30, 0.33, 0.21

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -70,7 +70,6 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 	//advance pixels
 	
 
-/*
 	for(int i = 0; i < h; i++)
 	{
 		for(int j = 0; j < w; j++)
@@ -78,12 +77,12 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 			dst->pixels[(y + i) * dst->w + x + j] = color;
 		}
 	}
-	*/
+	/*
 	for(int i = 0; i < h; i++)
 	{
 		memset(dst->pixels + (y + i) * dst->w + x, color, w);
 	}
-
+*/
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
