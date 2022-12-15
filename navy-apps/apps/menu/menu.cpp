b533ded51aca5ca58a902d7bc13dbafe6c8ae8e3
tracer-ics2022 <tracer@njuics.org>
2022-12-15 21:07:35 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:07:35 up  1:46,  1 user,  load average: 0.53, 0.27, 0.17

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -73,13 +73,11 @@ int main(int argc, char *argv[], char *envp[]) {
 
   font = new BDF_Font(font_fname);
   logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
-  printf("load over\n");
-   SDL_UpdateRect(logo_sf, 0, 0, 0, 0);
   assert(logo_sf);
-  //set_i_max();
+  set_i_max();
 
   while (1) {
-    /*display_menu(i_max);
+    display_menu(i_max);
 
     SDL_Event e;
     do {
@@ -115,7 +113,7 @@ int main(int argc, char *argv[], char *envp[]) {
       fprintf(stderr, "\033[31m[ERROR]\033[0m Exec %s failed.\n\n", exec_argv[0]);
     } else {
       fprintf(stderr, "Choose a number between %d and %d\n\n", 0, i_max);
-    }*/
+    }
   }
   return -1;
 }
