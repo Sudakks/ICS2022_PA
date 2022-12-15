485e0821c4a7c18e537089cc2faa1e322a9a482f
tracer-ics2022 <tracer@njuics.org>
2022-12-15 19:46:41 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:46:41 up 25 min,  1 user,  load average: 0.39, 0.23, 0.23

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -49,6 +49,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 	//往画布的指定矩形区域中填充指定的颜色
 	//即改变pixels里面的内容
+	/*
 	int x, y, w, h;
 	if(dstrect == NULL)
 	{
@@ -69,6 +70,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 			dst->pixels[(y + i) * dst->w + x + j] = color;
 		}
 	}
+	*/
 	/*
 	for(int i = 0; i < h; i++)
 	{
