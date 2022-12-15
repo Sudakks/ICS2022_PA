8b412fab2f23ffa855d5055694b637765045c998
tracer-ics2022 <tracer@njuics.org>
2022-12-15 16:55:56 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:55:56 up  2:42,  1 user,  load average: 0.12, 0.11, 0.09

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -18,7 +19,51 @@ int SDL_PollEvent(SDL_Event *ev) {
 
 int SDL_WaitEvent(SDL_Event *event) {
 	//success return 1, otherwise return 0
-  return 1;
+	//有type和sym两个变量
+	char buf[64];
+	char key_sym[32];
+  int ret = NDL_PollEvent(buf, sizeof(buf)); 
+	if(ret == 0)
+		return 0;// no key
+
+	uint8_t sym, type;
+	char* key_ptr = &buf[2];
+	while(*key_ptr == ' ')
+			key_ptr++;
+	//此时key_ptr指向的就应该是按键的类型
+
+	for(int i = 0; i < 32; i++, key_ptr++)
+	{
+		key_sym[i] = *key_ptr;
+		if(*key_ptr == '\0' || *key_ptr == '\n')
+		{
+			key_sym[i] = '\0';
+			break;
+		}
+	}
+
+	if(buf[0] == 'k' && buf[1] == 'd')
+	{
+		//keydown
+		type = SDL_KEYDOWN;
+	}
+	else if(buf[0] == 'k' && buf[1] == 'u')
+	{
+		type = SDL_KEYUP;
+	}
+	int sz = sizeof(keyname) / sizeof(char*);
+	for(int i = 0; i <= sz; i++)
+	{
+		if(strcmp(keyname[i], key_sym) == 0)
+		{
+			//find sym
+			sym = i;
+			break;
+		}
+	}
+	event->type = type;
+	event->key.keysym.sym = sym;
+	//这个应该对应着什么按键
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
