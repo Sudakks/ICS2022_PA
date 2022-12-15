89c2a19120e44b73bb7c24b5f1979d293763aedb
tracer-ics2022 <tracer@njuics.org>
2022-12-15 15:43:02 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:43:02 up  1:29,  1 user,  load average: 0.52, 0.38, 0.29

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -27,7 +27,9 @@ void render() {
   sprintf(fname, path, cur);
   slide = SDL_LoadBMP(fname);
   assert(slide);
+  printf("1111\n");
   SDL_UpdateRect(slide, 0, 0, 0, 0);
+  printf("2222\n");
 }
 
 void prev(int rep) {
