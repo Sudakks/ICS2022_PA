1bbe1d4e1b1588dc4254996a1582a521c8871535
tracer-ics2022 <tracer@njuics.org>
2022-12-15 14:54:45 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:54:45 up 41 min,  1 user,  load average: 0.65, 0.39, 0.29

--- navy-apps/libs/libbmp/src/SDL_bmp.c
+++ navy-apps/libs/libbmp/src/SDL_bmp.c
@@ -4,13 +4,10 @@
 
 SDL_Surface* SDL_LoadBMP(const char *filename) {
   int w, h;
-	printf("111\n");
   void *pixels = BMP_Load(filename, &w, &h);
   assert(pixels);
-	printf("222\n");
   SDL_Surface *s = SDL_CreateRGBSurfaceFrom(pixels, w, h, 32, w * sizeof(uint32_t),
       DEFAULT_RMASK, DEFAULT_GMASK, DEFAULT_BMASK, DEFAULT_AMASK);
   s->flags &= ~SDL_PREALLOC;
-	printf("333\n");
   return s;
 }
