e477573b48c5a41f64a148d988a86265fb0d25c2
tracer-ics2022 <tracer@njuics.org>
2022-12-15 21:49:56 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:49:56 up  2:28,  3 users,  load average: 0.29, 0.15, 0.14

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -13,13 +13,74 @@ int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
+
+bool get_event(SDL_Event *event)
+{
+	char buf[64];
+	char key_sym[32];
+  int ret = NDL_PollEvent(buf, sizeof(buf)); 
+	if(ret == 0)
+		return false;// no key
+
+	uint8_t sym, type;
+	char* key_ptr = &buf[2];
+	while(*key_ptr == ' ')
+			key_ptr++;
+	//此时key_ptr指向的就应该是按键的类型
+
+	for(int i = 0; i < 32; i++, key_ptr++)
+	{
+		key_sym[i] = *key_ptr;
+		if(*key_ptr == '\0' || *key_ptr == '\n')
+		{
+			key_sym[i] = '\0';
+			break;
+		}
+	}
+
+	if(buf[0] == 'k' && buf[1] == 'd')
+	{
+		//keydown
+		type = SDL_KEYDOWN;
+	}
+	else if(buf[0] == 'k' && buf[1] == 'u')
+	{
+		type = SDL_KEYUP;
+	}
+	int sz = sizeof(keyname) / sizeof(char*);
+	for(int i = 0; i <= sz; i++)
+	{
+		if(strcmp(keyname[i], key_sym) == 0)
+		{
+			//find sym
+			sym = i;
+			break;
+		}
+	}
+	event->type = type;
+	//printf("type = %d, sym = %d\n", type, sym);
+	event->key.keysym.sym = sym;
+	//这个应该对应着什么按键
+	return true;
+}
+
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+	if(get_event(ev))
+		return 1;
+	else
+		return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
 	//success return 1, otherwise return 0
 	//有type和sym两个变量
+	while(1)
+	{
+		if(get_event(event) == true)
+			return 1;
+	}
+	return 0;
+	/*
 	char buf[64];
 	char key_sym[32];
   int ret = NDL_PollEvent(buf, sizeof(buf)); 
@@ -65,6 +126,7 @@ int SDL_WaitEvent(SDL_Event *event) {
 	//printf("type = %d, sym = %d\n", type, sym);
 	event->key.keysym.sym = sym;
 	//这个应该对应着什么按键
+	*/
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
