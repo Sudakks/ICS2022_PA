c97ffbf3e0b8ecb3a482d6c79afb32f26b41b6e7
tracer-ics2022 <tracer@njuics.org>
2022-12-16 15:01:31 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:01:31 up 44 min,  1 user,  load average: 0.58, 0.34, 0.24

--- navy-apps/libs/libSDL_image/src/stb_image.h
+++ navy-apps/libs/libSDL_image/src/stb_image.h
@@ -867,6 +867,7 @@ static void stbi__vertical_flip_slices(void *image, int w, int h, int z, int byt
 static unsigned char *stbi__load_and_postprocess_8bit(stbi__context *s, int *x, int *y, int *comp, int req_comp)
 {
    stbi__result_info ri;
+	 printf("huhuhuhuhuhu\n");
    void *result = stbi__load_main(s, x, y, comp, req_comp, &ri, 8);
 	 printf("huhuhu\n");
 
