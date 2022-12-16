361601b760bca05f3769e3a212c712be9d9805cf
tracer-ics2022 <tracer@njuics.org>
2022-12-16 12:19:55 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:19:55 up  2:00,  1 user,  load average: 0.08, 0.27, 0.31

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -90,10 +90,12 @@ size_t fs_read(int fd, void *buf, size_t len)
 	if(ret < 0)
 		return -1;
 	size_t read_sz = ramdisk_read(buf, disoff + off, ret);
+	printf("-------------\n");
 	if(read_sz > 1000)
 		printf("bigger\n");
 	else
 		printf("less\n");
+	printf("--------------\n");
 	file_table[fd].open_offset += read_sz;
 	assert(read_sz == ret);
 	//这个是相对于这个文件头的偏移量
