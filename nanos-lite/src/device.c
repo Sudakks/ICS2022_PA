c02ac6e9bafafeb0019f58d642312b6f280470a1
tracer-ics2022 <tracer@njuics.org>
2022-12-18 14:16:10 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:16:10 up  4:59,  1 user,  load average: 0.12, 0.25, 0.25

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -63,7 +63,6 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
 	//write len bytes from buf to the offset of screen
-	printf("yes\n");
 	int w = io_read(AM_GPU_CONFIG).width;
 	int x = (offset) % w;
 	int y = (offset) / w;
