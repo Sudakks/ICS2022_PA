f962185c818672be548aa4717deb8a319007d1a7
tracer-ics2022 <tracer@njuics.org>
2022-12-18 13:54:33 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:54:33 up  4:38,  1 user,  load average: 0.20, 0.18, 0.13

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -187,12 +187,12 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 			for(int j = 0; j < w; j++)
 			{
 				uint32_t pos = (y + i) * s->w + x + j;
-				SDL_Color col = s->format->palette->colors[*test++];
-				printf("pos = %d, pix = %d\n", pos, *test);
+				SDL_Color *col = &s->format->palette->colors[*test++];
+				printf("pos = %d, pix = %d\n", (s->w * (i + y) + x + j), *test);
 //s->format->palette->colors[pixels[坐标索引]]
 	//			if(col.r != 0 && col.g != 0)
 //					printf("pos = %d, r = %d, g = %d, b = %d\n", w *i + j, col.r, col.g, col.b);
-				pix[w * i + j] = (col.a << 24) | (col.b << 16) | (col.g << 8) | col.b;
+				pix[w * i + j] = (col->r << 16) | (col->g << 8) | col->b;
 			}
 		}
 		NDL_DrawRect(pix, x, y, w, h);
