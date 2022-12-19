81b14d0c7f73cc72c2df2ad056f413826e841d2d
tracer-ics2022 <tracer@njuics.org>
2022-12-19 09:55:41 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:55:41 up 41 min,  1 user,  load average: 0.67, 0.58, 0.40

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -143,6 +143,7 @@ static void draw_text_row(char *s, int r) {
 }
 
 static void display_menu(int n) {
+printf("iojnjbj\n");
   clear_display();
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
