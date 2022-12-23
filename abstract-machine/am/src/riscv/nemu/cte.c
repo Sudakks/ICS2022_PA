700aff074380b33165f198668b1aff77cd79f40e
tracer-ics2022 <tracer@njuics.org>
2022-12-23 16:49:40 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:49:40 up  2:00,  1 user,  load average: 0.08, 0.11, 0.04

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -47,6 +47,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 	Context* con = (Context*)(kstack.end - sizeof(Context));
 	con->mepc = (uintptr_t)entry;
 	printf("kcontext = %p\n", entry);
+	printf("con->mepc = %p\n", con->mepc);
 	/*刚好指的就是一个函数的入口地址*/
 	/*should add other things?*/
 	/*应该是PCB中的cp指针*/
