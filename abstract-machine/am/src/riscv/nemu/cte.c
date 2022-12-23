d1c6400dbd05551034f29a664cdb74ad9d892498
tracer-ics2022 <tracer@njuics.org>
2022-12-23 18:23:24 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:23:24 up  3:34,  1 user,  load average: 0.23, 0.17, 0.10

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -49,7 +49,9 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 	//将arg保存在函数参数寄存器里面
 	//printf("lcontext : arg=%c\n", *(char*)arg);
 	//参数传进来是正确的
+	printf("yes\n");
 	con->gpr[11] = (uintptr_t)arg;
+	printf("no\n");
 	//printf("kcontext = %p\n", entry);
 	/*刚好指的就是一个函数的入口地址*/
 	/*should add other things?*/
