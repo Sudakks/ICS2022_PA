abb4931aa2c0133d600142c72816bf0f09a27189
tracer-ics2022 <tracer@njuics.org>
2022-12-26 18:57:18 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:57:18 up  4:40,  1 user,  load average: 0.30, 0.20, 0.16

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -5,6 +5,7 @@
 #include <time.h>
 #include "syscall.h"
 #include <stdio.h>
+#include <string.h>
 
 // helper macros
 #define _concat(x, y) x ## y
@@ -112,7 +113,7 @@ int _gettimeofday(struct timeval *tv, struct timezone *tz) {
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
 	if(strcmp(fname, "/bin/exec-test") == 0)
-		printf("uiuiui\n");
+		assert(0);
   return _syscall_(SYS_execve, (uintptr_t)fname, (uintptr_t)argv, (uintptr_t)envp);
 }
 
