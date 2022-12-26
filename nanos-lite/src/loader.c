0c1407629eca860a814b6ccd9d937f034cbde853
tracer-ics2022 <tracer@njuics.org>
2022-12-26 15:33:07 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:33:07 up  1:15,  1 user,  load average: 0.15, 0.11, 0.09

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -118,7 +118,10 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 	if(argv)
 	{
 		while(argv[argc])
+		{
+			printf("argv[%d] = %p\n", argv[argc]);
 			argc++;
+		}
 	}
 	if(envp)
 	{
