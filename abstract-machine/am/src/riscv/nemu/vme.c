9b46bfc2099c478a3f20a9e270693fdf281602dd
tracer-ics2022 <tracer@njuics.org>
2022-12-28 16:50:53 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:50:53 up  2:31,  1 user,  load average: 0.07, 0.10, 0.09

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -14,11 +14,13 @@ static Area segments[] = {      // Kernel memory mappings
 #define USER_SPACE RANGE(0x40000000, 0x80000000)
 
 static inline void set_satp(void *pdir) {
+	printf("run into set_satp\n");
   uintptr_t mode = 1ul << (__riscv_xlen - 1);
   asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
 }
 
 static inline uintptr_t get_satp() {
+	printf("run into get_satp\n");
   uintptr_t satp;
   asm volatile("csrr %0, satp" : "=r"(satp));
   return satp << 12;
