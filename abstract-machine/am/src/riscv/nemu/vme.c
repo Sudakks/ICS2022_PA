88407ef81b1c9317304317ba5054e890f5ee980c
tracer-ics2022 <tracer@njuics.org>
2022-12-30 09:30:47 +0800

    >  compile NEMU 211240047 徐研 Linux sudakks-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:30:47 up 23 min,  1 user,  load average: 0.15, 0.21, 0.26

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -38,6 +38,7 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
   for (i = 0; i < LENGTH(segments); i ++) {
     void *va = segments[i].start;
     for (; va < segments[i].end; va += PGSIZE) {
+			printf("va = %x, PGSIZE = %d\n", va, PGSIZE);
       map(&kas, va, va, 0);
     }
   }
